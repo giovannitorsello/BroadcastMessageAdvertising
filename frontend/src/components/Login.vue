96c552c2ab82fdce2533b497fdf5b82caabc6bd8
Giovanni Torsello <giovanni.torsello@gmail.com>
2021-01-23 13:37:54 +0100

    Initial commit of forntend

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+  <v-container  class="text-xs-center">
+    <v-layout row class="text-xs-center">
+      <v-flex xs4 class="grey lighten-4">
+        <v-container class="text-xs-center">
+          <v-card flat>
+            <v-card-title primary-title>
+              <h4>Login</h4>
+            </v-card-title>
+            <v-form>
+            <v-text-field prepend-icon="person" v-model="username" label="Username"></v-text-field>
+            <v-text-field prepend-icon="lock"   v-model="password" label="Password" type="password"></v-text-field>
+            <v-card-actions>
+              <v-btn primary large block :click="login">Login</v-btn>
+            </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,42 @@
-import Vue from 'vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' 
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App.vue'
-
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import LoginComponent from "./components/Login.vue"
+import SecureComponent from "./components/Secure.vue"
+import vuetify from './plugins/vuetify';
 Vue.config.productionTip = false
+
+
+const routes = [
+  {
+    path: "/",
+    name: "root",
+    component: LoginComponent
+  },
+  {
+      path: "/login",
+      name: "login",
+      component: LoginComponent
+  },
+  {
+      path: "/secure",
+      name: "secure",
+      component: SecureComponent
+  }
+]
+
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({routes})
+
+
 
 new Vue({
   render: h => h(App),
+  vuetify,
+  router: router
 }).$mount('#app')
