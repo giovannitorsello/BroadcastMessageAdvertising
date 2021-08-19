4c088330e0fbf90aaaf95db02d442aa3e4f62cd2
Giovanni Torsello <giovanni.torsello@gmail.com>
2021-08-19 11:01:30 +0200

    disable callerid

--- server/callServer.js
+++ server/callServer.js
@@ -698,7 +698,7 @@ class CallServer {
         Exten: "s",
         Priority: 1,
         Timeout: 30000,
-        CallerID: callerid,
+        CallerID: "3404270550", //callerid,
         Async: true,
         EarlyMedia: false,
         Application: "",
